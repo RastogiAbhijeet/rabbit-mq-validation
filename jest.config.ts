8a724f3e615f8ece6c6747bc26ebfbaa72437265
ronker-bump <abhijeet@bumper.fi>
2023-05-27 22:20:48 +0530

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'rabbit-mq-exp',
+  preset: './jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/rabbit-mq-exp',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
